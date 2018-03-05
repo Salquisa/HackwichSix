5102545ecc6cd72e822ed68c195ca5e92ea58ffa
CM Student <cmstudent@UHWO-ACM-C-07.local>
2018-03-05 10:34:28 -1000

    Completed HackwichSix - 3/5/18
    Completed the following steps:
    Part 9: Let’s Print the Value of the Slider in the Alert View
    Part 10: Display the slider’s current value in the alert view message

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var Slider: UISlider!
+//create a variable that can hold the value of the slider
+    var currentValue: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,10 +29,14 @@ class ViewController: UIViewController {
     @IBAction func sliderHasMoved(_ sender: Any) {
         
         print("The value of the slider is:\(Slider.value)")
+        currentValue = lroundf(Slider.value)
     }
     @IBAction func myGuessButtonPressed(_ sender: Any) {
+    //5. New variable message the displays a message that includes currentValue
+        let message = "The value is: \(currentValue)"
+        
     //1. create alert view
-        let alert = UIAlertController(title:"Hello World!", message: "This is my first alert view", preferredStyle: .alert)
+        let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
         
     //2. button that user taps to dismiss view controller
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
