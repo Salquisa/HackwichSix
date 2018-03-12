4b993ae15ffd1c37bd30447c7ef6e1d8603d2fbb
CM Student <cmstudent@UHWO-ACM-C-07.local>
2018-03-12 09:53:27 -1000

    3/12/18 - Completed: Part 1: Set CurrentValue to the slider’s value in ViewDidLoad. Part 2: Game Play: Setting Up a Target Value.

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -14,10 +14,18 @@ class ViewController: UIViewController {
 //create a variable that can hold the value of the slider
     var currentValue: Int = 0
     
+//create a variable to hold the target value
+    var targetValue: Int = 1
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(Slider.value)
+        
+        //Set targetValue to a randomly generated number that the acr4random_uniform function.
+        
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,7 +41,7 @@ class ViewController: UIViewController {
     }
     @IBAction func myGuessButtonPressed(_ sender: Any) {
     //5. New variable message the displays a message that includes currentValue
-        let message = "The value is: \(currentValue)"
+        let message = "The value is: \(currentValue)" + "\nThe target value is: \(targetValue)"
         
     //1. create alert view
         let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
